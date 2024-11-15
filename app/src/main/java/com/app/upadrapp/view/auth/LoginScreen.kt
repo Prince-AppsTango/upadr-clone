d2bdcc83836536772396e69fff6aec00b5e88dcc
Prince Kumar Yadav <princekumar@Princes-MacBook-Pro.local>
2024-11-15 21:22:54 +0530

    add login and signup screen

--- app/src/main/java/com/app/upadrapp/view/auth/LoginScreen.kt
+++ app/src/main/java/com/app/upadrapp/view/auth/LoginScreen.kt
@@ -1,11 +1,29 @@
 package com.app.upadrapp.view.auth
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
+import com.app.upadrapp.ui.theme.LightCyan
+import com.app.upadrapp.utils.SafeArea
 
 @Composable
 fun LoginScreen(navController: NavController) {
-    Text(text = "Login Screen", color = Color.White)
+    SafeArea {
+        Column(
+            modifier = Modifier
+                .background(LightCyan)
+                .fillMaxSize(1f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Login Screen", color = Color.Black)
+        }
+    }
 }
