64db19e6b7efec1aec3ae6e9efe7ce1e54a9bdbd
Prince Kumar Yadav <princekumar@Princes-MacBook-Pro.local>
2024-11-16 18:49:19 +0530

    removed unused code

--- app/src/main/java/com/app/upadrapp/view/app/HomePage.kt
+++ app/src/main/java/com/app/upadrapp/view/app/HomePage.kt
@@ -3,10 +3,8 @@ package com.app.upadrapp.view.app
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 import com.app.upadrapp.shared.TopDrawerNavigation
 
