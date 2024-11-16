d10f0cdaea6fbdc4476aabb4af7d4aaaff1579a6
Prince Kumar Yadav <princekumar@Princes-MacBook-Pro.local>
2024-11-16 12:29:45 +0530

    login screen done

--- app/src/main/java/com/app/upadrapp/view/auth/LoginScreen.kt
+++ app/src/main/java/com/app/upadrapp/view/auth/LoginScreen.kt
@@ -2,8 +2,11 @@ package com.app.upadrapp.view.auth
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,9 +14,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -22,14 +23,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.app.upadrapp.R
+import com.app.upadrapp.shared.CustomButton
 import com.app.upadrapp.shared.CustomTextField
 import com.app.upadrapp.shared.Subtitle
 import com.app.upadrapp.shared.Title
 import com.app.upadrapp.ui.theme.Black
 import com.app.upadrapp.ui.theme.LightCyan
+import com.app.upadrapp.ui.theme.MediumTurquoise
 import com.app.upadrapp.ui.theme.SubTitleColor
 import com.app.upadrapp.utils.SafeArea
 
@@ -81,6 +85,25 @@ fun LoginScreen(navController: NavController) {
                     CustomTextField(text = "Password", value = password.value, onChangeValue = {
                         password.value = it
                     })
+                    Spacer(modifier = Modifier.height(5.dp))
+                    Text(text = "Forgot Password?", modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { }, textAlign = TextAlign.End, color = MediumTurquoise)
+                    Spacer(modifier = Modifier.height(30.dp))
+                    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                        CustomButton(text = "Login", width = 300, onClick = {
+                        })
+                    }
+                    Spacer(modifier = Modifier.height(25.dp))
+                    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxWidth()) {
+                        Image(painter = painterResource(id = R.drawable.googlelogo), contentDescription = "googleLogo" , modifier = Modifier
+                            .size(70.dp)
+                            .clickable { })
+                    }
+                    Row(modifier = Modifier.fillMaxSize(1f).padding(0.dp,0.dp,0.dp,20.dp), verticalAlignment = Alignment.Bottom, horizontalArrangement = Arrangement.Center) {
+                       Subtitle(text = "Don’t have an account yet? ", color = SubTitleColor)
+                       Subtitle(text = "Sign up", color = MediumTurquoise)
+                    }
                 }
             }
         }
