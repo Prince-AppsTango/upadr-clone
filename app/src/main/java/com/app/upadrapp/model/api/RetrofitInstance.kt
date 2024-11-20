554102edc49f9158ccc3b31b07827a9205df1dcc
Prince Kumar Yadav <princekumar@Princes-MacBook-Pro.local>
2024-11-20 12:20:55 +0530

    working on auth

--- app/src/main/java/com/app/upadrapp/model/api/RetrofitInstance.kt
+++ app/src/main/java/com/app/upadrapp/model/api/RetrofitInstance.kt
@@ -1,16 +1,31 @@
 package com.app.upadrapp.model.api
 
+import android.content.Context
 import com.app.upadrapp.model.api.authservices.AuthServices
+import com.app.upadrapp.store.getAccessTokenSync
 import com.app.upadrapp.utils.Constant
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(Constant.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+    fun getInstance(context: Context): Retrofit {
+        val client = OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val accessToken = getAccessTokenSync(context)
+                val request = chain.request().newBuilder().apply {
+                    addHeader("Authorization", "Bearer $accessToken")
+                }.build()
+                chain.proceed(request)
+            }
+            .build()
+        return Retrofit.Builder()
+            .baseUrl(Constant.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
     }
-    val getSignIn: AuthServices by lazy {
-        retrofit.create(AuthServices::class.java)
+    fun getSignIn(context: Context): AuthServices {
+        return getInstance(context).create(AuthServices::class.java)
     }
 }
--- app/src/main/java/com/app/upadrapp/store/DataStoreManager.kt
+++ app/src/main/java/com/app/upadrapp/store/DataStoreManager.kt
@@ -5,7 +5,9 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
 
 val Context.dataStore by preferencesDataStore(name = "settings")
 
@@ -33,5 +35,14 @@ fun getAccessToken(context: Context): Flow<String> {
 fun getRefreshToken(context: Context): Flow<String> {
     return context.dataStore.data.map { preferences ->
         preferences[REFRESH_TOKEN]?:""
+    }
+}
+
+// copy from the google
+fun getAccessTokenSync(context: Context): String {
+    return runBlocking {
+        context.dataStore.data
+            .map { preferences -> preferences[ACCESS_TOKEN] ?: "" }
+            .first()
     }
 }
